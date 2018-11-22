261e4284e1817112d2ed45b6996effa5064bd78b
Bakatkin <bakatkin@gmail.com>
2018-11-22 14:31:02 +0300

    ДЗ1

---
+++ lesson_1/src/lesson1/MainClass.java
@@ -0,0 +1,42 @@
+package lesson1;
+
+import lesson1.Competitors.*;
+import lesson1.obstacles.Cross;
+import lesson1.obstacles.Obstacle;
+import lesson1.obstacles.Wall;
+import lesson1.obstacles.Water;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        //Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик"), new Cat ("Пушистик"), new Dog ("Рэкс")};
+        Obstacle[] obstacles = {new Cross(400), new Wall(2), new Water(1)};
+        Cat cat = new Cat ("Пушок");
+
+
+       /* for (Competitor c : competitors) {
+            for (Obstacle o : obstacles) {
+                o.doIt(c);
+                if (!c.isOnDistance()) break;
+            }
+        }
+
+        System.out.println("========================");
+        for (Competitor c : competitors) {
+            c.showResult();
+        }*/
+
+       Team team = new Team("DINAMO", new Human("Боб"), new Cat("Барсик"), new Dog("Бобик"), new Cat ("Пушистик"));
+       Course course = new Course(obstacles);
+
+        team.teamInfo();        //общая информация о команде
+        System.out.println("____________________");
+        course.doIt(team);      //команда проходит полосу препятствий
+        System.out.println("_____________________");
+        team.teamInfo();
+        System.out.println("________________");
+        team.showResult();      //результаты прохождения всех участников команды
+        System.out.println("________________");
+        team.winCompetitor();   //участники, которые прошли полосу препятствий
+    }
+}
