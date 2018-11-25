0556bb6911b977be62a1534920a5c172cbb5201b
Bakatkin <bakatkin@gmail.com>
2018-11-25 20:07:30 +0300

    Сделал обработку исключений

--- lesson_2/src/ArrayConvert.java
+++ lesson_2/src/ArrayConvert.java
@@ -1,15 +1,23 @@
 public class ArrayConvert {
+
     private String [][] strArray;
 
     public ArrayConvert(String[][] strArray) {
         this.strArray = strArray;
+        if (strArray.length != 4 || strArray[0].length != 4){
+            throw new MySizeArrayException("Массив иного размера, чем 4 х 4");
+        }
     }
 
     public int convertAndSum (){
         int sum = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                sum = sum + Integer.parseInt(strArray[i][j]);
+                try {
+                    sum = sum + Integer.parseInt(strArray[i][j]);
+                } catch (NumberFormatException e){
+                    throw new MyArrayDataException("Ошибка в ячейке " + i + " " + j);
+                }
             }
         }
         return sum;
